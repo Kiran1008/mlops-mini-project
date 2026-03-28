b70f8f6f318f3201344dea5e09612330b2bd07f9
Kiran Kumar <tagaramkirankumar1008@gmail.com>
2026-03-28 20:29:24 +0530

    connect to  dagshub server

---
+++ notebooks/dagshub_setup.py
@@ -0,0 +1,10 @@
+import dagshub
+import mlflow
+
+mlflow.set_tracking_uri("https://dagshub.com/Kiran1008/mlops-mini-project.mlflow")
+
+dagshub.init(repo_owner='Kiran1008', repo_name='mlops-mini-project', mlflow=True)
+
+with mlflow.start_run():
+  mlflow.log_param('parameter name', 'value')
+  mlflow.log_metric('metric name', 1)
